ff17201b92e034a12b998587e1ba9d59b46cd251
Amirhoseinjkar <150539608+Amirhoseinjkar@users.noreply.github.com>
2024-07-12 17:39:44 +0330

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import aboutSvg from '../assets/about.svg';
+import SectionTitle from './SectionTitle';
+
+import React from 'react'
+
+const About = () => {
+  return (
+<section className='bg-white py-20 'id='about'>
+<div className='align-element grid md:grid-cols-2 items-center gap-16'>
+  <img src={aboutSvg} className='w-full h-64 ' />
+  <article>
+    <SectionTitle text='code and coffee'/>
+    <p className='text-slate-600 mt-8 leading-loose'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, aliquam. Corporis impedit adipisci dolore nulla. Omnis, impedit aperiam! Vero quis delectus esse reiciendis consequuntur doloremque porro magnam. Officiis, qui repellat?</p>
+  </article>
+</div>
+</section>
+    )
+}
+
+export default About
